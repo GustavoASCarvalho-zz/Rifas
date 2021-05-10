7c7f915c91b189101215b93a80b54c3a12cf45eb
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2021-05-10 16:34:03 -0300

    first commit

---
+++ app/Controllers/Http/IndicesController.ts
@@ -0,0 +1,7 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class IndicesController {
+  public async index({ view }: HttpContextContract) {
+    return view.render('index')
+  }
+}
