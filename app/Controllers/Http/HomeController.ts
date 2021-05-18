df4a4c869d458fdbd337140ecff8a8873ba6b3e4
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2021-05-18 14:53:26 -0300

    Update view index

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -1,8 +1,10 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Raffle from 'App/Models/Raffle'
 
 export default class HomeController {
   public async index({ view }: HttpContextContract) {
-    return view.render('home/index')
+    const raffles = await Raffle.query()
+    return view.render('home/index', { raffles })
   }
   public async about({ view }: HttpContextContract) {
     return view.render('home/about')
