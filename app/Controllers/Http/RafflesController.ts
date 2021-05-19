ea97854ec20b07b32568528790574d9bb45a2778
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2021-05-19 14:14:45 -0300

    Add create raffle

--- app/Controllers/Http/RafflesController.ts
+++ app/Controllers/Http/RafflesController.ts
@@ -1,12 +1,37 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Raffle from 'App/Models/Raffle'
+import Type from 'App/Models/Type'
 
 export default class RafflesController {
   public async index({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
+  public async create({ view }: HttpContextContract) {
+    const types = await Type.query()
+    return view.render('raffles/create', { types })
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ response, request, session, auth }: HttpContextContract) {
+    const data = await request.only([
+      'typeId',
+      'title',
+      'ticketPrize',
+      'description',
+      'probableRaffleDate',
+      'initialSaleDate',
+      'endSaleDate',
+    ])
+
+    try {
+      const user = auth.user
+      await user?.related('raffles').create(data)
+    } catch (error) {
+      session.flash('errors', 'Erro no cadastro. Verifique suas informações.')
+      console.log(error)
+
+      return response.redirect().toRoute('raffles.create')
+    }
+    response.redirect().toRoute('/')
+  }
 
   public async show({ view, params }: HttpContextContract) {
     const raffle = await Raffle.query().where('id', params.id).firstOrFail()
@@ -19,5 +44,3 @@ export default class RafflesController {
 
   public async destroy({}: HttpContextContract) {}
 }
-
-//const raffle = await Raffle.query().where('id', params.id)
